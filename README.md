8b5ba400a5064ed2b5ceb6be37d4e46c81b36554
falsandtru <falsandtru@users.noreply.github.com>
2018-05-24 11:57:46 +0900

    chore: version 0.0.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # atomic-promise
+
+[![Build Status](https://travis-ci.org/falsandtru/atomic-promise.svg?branch=master)](https://travis-ci.org/falsandtru/atomic-promise)
+
+Promise with no delay.
---
+++ test/interface/package.ts
@@ -0,0 +1,11 @@
+import { AtomicPromise } from '../../index';
+
+describe('Interface: Package', function () {
+  describe('AtomicPromise', function () {
+    it('AtomicPromise', function () {
+      assert(typeof AtomicPromise === 'function');
+    });
+
+  });
+
+});
