773ed5ff127d769857fd3b4b30298961692c1ada
falsandtru <falsandtru@users.noreply.github.com>
2021-03-10 06:01:38 +0900

    Refactoring

--- test/interface/package.ts
+++ test/interface/package.ts
@@ -1,12 +1,16 @@
 import { AtomicPromise } from '../../index';
 
 describe('Interface: Package', function () {
+  describe('global', function () {
+    it('global', function () {
+      assert(global['AtomicPromise'] !== AtomicPromise);
+    });
+  });
+
   describe('AtomicPromise', function () {
     it('AtomicPromise', function () {
       assert(typeof AtomicPromise === 'function');
-      assert(typeof window.AtomicPromise === 'function');
     });
-
   });
 
 });
